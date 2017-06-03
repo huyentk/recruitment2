712761d330157c33b9a89f801eeed0ec01034ba5
Huyen Tran <huyentk1296@gmail.com>
2017-06-04 06:57:42 +0700

    commit change

--- resources/views/jobs/jobs_list.blade.php
+++ resources/views/jobs/jobs_list.blade.php
@@ -17,18 +17,12 @@
             <hr style="margin-top: 0px;margin-bottom: 0px;margin-right: 20px;"/>
             <form method="post" action="{{ route('post-jobs-list') }}">
                 <div class="row" style="margin-top: 15px; padding-bottom: 20px;">
-                    <div class="col-md-6">
+                    <div class="col-md-10">
                         <div class="input-group">
                             <span class="input-group-addon"><i class="glyphicon glyphicon-tag" aria-hidden="true"></i></span>
                             <input type="text" id="skill_tags" class="form-control" placeholder="Search for your skill (i.e: PHP, Python,...)" name="skill">
                         </div>
                     </div>
-                    {{--<div class="col-md-4">--}}
-                        {{--<div class="input-group">--}}
-                            {{--<span class="input-group-addon"><i class="glyphicon glyphicon-map-marker" aria-hidden="true"></i></span>--}}
-                            {{--<input type="text" id="city" class="form-control" placeholder="City..." name="city">--}}
-                        {{--</div>--}}
-                    {{--</div>--}}
                     <div class="col-md-2">
                         <input type="hidden" name="_token" value="{{ Session::token() }}">
                         <button type="submit" class="btn" style="width: 135px;text-align: center;background-color: #e58618;color: white;"><span class="glyphicon glyphicon-search"></span> Search</button>
